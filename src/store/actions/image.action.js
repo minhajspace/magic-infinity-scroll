18a271f68dc49c41e67789645c60417c98d3c762
Minhaj Ahmad Khan <51471123+minhajspace@users.noreply.github.com>
2022-02-01 19:28:16 +0530

    change access key

--- src/store/actions/image.action.js
+++ src/store/actions/image.action.js
@@ -5,7 +5,7 @@ export function fetchImage (page,params){
     return  async (dispatch) => {
         dispatch({type:FETCH_RANDOM_IMAGE_START})
      try{
-      const response =await axios.get(`https://api.unsplash.com/search/photos/?page=${page}&query=${params}&client_id=rKp1E7a0U0BgA1ia34UUHTVUKlTUUzZh9Ke_FpHcyao&per_page=10&order_by=relevant;`)
+      const response =await axios.get(`https://api.unsplash.com/search/photos/?page=${page}&query=${params}&client_id=6QDxVPT42nrcJs5H9TdPmcLCJaiM7KVby-Xvg0B9Ke0&per_page=10&order_by=relevant;`)
       dispatch({type:FETCH_RANDOM_IMAGE_SUCCESS,payload:response.data.results})
      }catch(err){ 
       dispatch({type:FETCH_RANDOM_IMAGE_FAILED,err:err.response.data})
